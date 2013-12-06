e21b298a1b13553ed0416a98c8d97450005ecbc2
billyfbrain <billyfbrain@gmail.com>
2013-12-06 17:01:15 +0400

    Create fork Flask-FlatPages. Flask-FlastPagesCut.

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ from setuptools import setup, find_packages
 
 ROOT = os.path.dirname(__file__)
 README = open(os.path.join(ROOT, 'README')).read()
-INIT_PY = open(os.path.join(ROOT, 'flask_flatpages', '__init__.py')).read()
+INIT_PY = open(os.path.join(ROOT, 'flask_flatpagescut', '__init__.py')).read()
 VERSION = re.search("VERSION = '([^']+)'", INIT_PY).group(1)
 
 
 setup(
-    name='Flask-FlatPages',
+    name='Flask-FlatPagesCut',
     version=VERSION,
-    url='https://github.com/SimonSapin/Flask-FlatPages',
+    url='https://github.com/billyfbrain/Flask-FlatPagesCut',
     license='BSD',
-    author='Simon Sapin',
-    author_email='simon.sapin@exyr.org',
-    description='Provides flat static pages to a Flask application',
+    author='Vasily Klyuev',
+    author_email='billyfbrain@gmail.com',
+    description='Fork Flask-FlatPages (Provides flat static pages to a Flask application)',
     long_description=README,
     packages=find_packages(),
     # test pages
     package_data={'': ['pages*/*.*', 'pages/*/*.*', 'pages/*/*/*.*']},
-    test_suite='flask_flatpages.tests',
+    test_suite='flask_flatpagescut.tests',
     zip_safe=False,
     platforms='any',
     install_requires=[
